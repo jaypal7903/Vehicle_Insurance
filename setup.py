bd33b3511ade9e42f3934e7f09dbecb0a0a3bcc7
jaypal7903 <techjaypal007@gmail.com>
2026-08-16 01:12:10 +0530

    the vitual environment created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= "src",
+    version= "0.0.1",
+    author="jaypal rathod",
+    author_email= "techjaypal007@gmail.com",
+    packages= find_packages()   
+)
