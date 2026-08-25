9064aa43f6497ab32d37c697bc1a0727aa112491
jaypal7903 <techjaypal007@gmail.com>
2026-08-25 21:50:55 +0530

    the data-validation,transformation,and upto trining related task completed

---
+++ src/components/data_transformation.py
@@ -0,0 +1,181 @@
+import sys
+import numpy as np
+import pandas as pd
+from imblearn.combine import SMOTEENN
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.compose import ColumnTransformer
+
+from src.constants import TARGET_COLUMN, SCHEMA_FILE_PATH, CURRENT_YEAR
+from src.entity.config_entity import DataTransforamationConfig
+from src.entity.artifact_entity import DataTransformationArtifact, DataIngestionArtifact, DataValidationArtifact
+from src.exception import MyException
+from src.logger import logging
+from src.utils.main_utils import save_object, save_numpy_array_data, read_yaml_file
+
+class DataTransformation:
+    def __init__(self, data_ingestion_artifact: DataIngestionArtifact, 
+                 data_transformation_config: DataTransforamationConfig,
+                 data_validation_artifact: DataValidationArtifact) :
+        try:
+            self.data_ingestion_artifact = data_ingestion_artifact
+            self.data_transformation_config = data_transformation_config
+            self.data_validation_artifact = data_validation_artifact
+            self._schema_config = read_yaml_file(file_path=SCHEMA_FILE_PATH)
+        except Exception as e:
+            raise MyException(e,sys)
+
+    @staticmethod
+    def read_data(file_path) -> pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise MyException(e,sys)
+
+    def get_data_transfer_object(self) -> Pipeline :
+        """
+        Creates and returns a data transformer object for the data,
+        including genger mapping, dummy variable creation, column renaming,
+        feature scaling, and type adjustments.
+        """
+        logging.info("Entered get_data_transfer_object method od DataTransformation class")
+
+        try:
+            # Initialize transformers
+            numerical_transformer = StandardScaler()
+            min_max_scaler = MinMaxScaler()
+            logging.info("Transformers Initialized : standrdScaler-MinMaxScaler")
+
+            # load schema configurations
+            num_features = self._schema_config['num_features']
+            mm_columns = self._schema_config['mm_columns']
+            logging.info("Cols loaded from schema.")
+
+            # Creating Preprocessing pipeline
+            preprocesser = ColumnTransformer(
+                transformers=[
+                    ("StandrdScaler",numerical_transformer, num_features),
+                    ("MinMaxScaler",min_max_scaler,mm_columns)
+                ],
+                remainder='passthrough'  # Leaves other column as they are
+            )
+
+            # Wrapping everything in a single pipeline
+            final_pipeline = Pipeline(steps=[("Preprocessor", preprocesser)])
+            logging.info("Final pipeline is ready!!")
+            logging.info("Exited get_data_transfer_object method of DataTransformation class")
+            return final_pipeline
+        except Exception as e:
+            raise MyException(e,sys) from e
+
+    def _map_gender_column(self,df) :
+        """
+        Map gender column to 0 for Female and 1 for Male.
+        """
+        logging.info("Mapping Gender column to binary value")
+        df['Gender'] = df['Gender'].map({'Female': 0, 'Male': 1}).astype('int')
+        return df
+
+    def _create_dummy_column(self,df) :
+        """
+        Create dummy variables for categorical features.
+        """
+        logging.info("creating dummy variables for categorical features.")
+        df = pd.get_dummies(df,drop_first=True)
+        return df
+
+    def _rename_columns(self,df) :
+        """
+        Rename specific columns and ensure integer types for dummy columns.
+        """
+        logging.info("renaming specific columns and casting to int")
+        df = df.rename(columns={
+            "Vehicle_Age_< 1 Year" : "Vehicle_Age_lt_1_Year",
+            "Vehicle_Age_> 2 Year" : "Vehicle_Age_gt_2_Years"
+        })
+        for col in ["Vehicle_Age_lt_1_Year", "Vehicle_Age_gt_2_Year","Vehicle_Damage_Yes"]:
+            if col in df.columns:
+                df[col] = df[col].astype('int')
+        return df
+
+    def _drop_id_column(self,df):
+        """
+        Drop the 'id' columns if it is exists.
+        """
+        logging.info("Droping the 'id' column")
+        drop_col = self._schema_config['drop_columns']
+        if drop_col in df.columns:
+            df = df.drop(drop_col, axis=1)
+        return df
+
+    def initiate_data_transformation(self) -> DataTransformationArtifact:
+        """
+        Initiate the data transformation component for the pipeline.
+        """
+        try:
+            logging.info("Data transforamtion started !!")
+            if not self.data_validation_artifact.validation_status :
+                raise Exception(self.data_validation_artifact.message)
+
+            # Load train and test data
+            train_df = self.read_data(file_path=self.data_ingestion_artifact.trained_file_path)
+            test_df = self.read_data(file_path=self.data_ingestion_artifact.test_file_path)
+            logging.info("Train-Test data loaded")
+
+            input_features_train_df = train_df.drop(TARGET_COLUMN, axis=1)
+            target_features_train_df = train_df[TARGET_COLUMN]
+
+            input_features_test_df = test_df.drop(TARGET_COLUMN,axis=1)
+            target_features_test_df = test_df[TARGET_COLUMN]
+            logging.info("Input and target cols defined for both train and test df. ")
+
+            # Apply column transformations in specified sequence
+            input_features_train_df = self._map_gender_column(input_features_train_df)
+            input_features_train_df = self._drop_id_column(input_features_train_df)
+            input_features_train_df = self._create_dummy_column(input_features_train_df)
+            input_features_train_df = self._rename_columns(input_features_train_df)
+
+            input_features_test_df = self._map_gender_column(input_features_test_df)
+            input_features_test_df = self._drop_id_column(input_features_test_df)  
+            input_features_test_df = self._create_dummy_column(input_features_test_df)
+            input_features_test_df = self._rename_columns(input_features_test_df)
+            logging.info("Custom transformation applied to train and test data")
+
+            logging.info("Starting data transformation")
+            preprocesser = self.get_data_transfer_object()
+            logging.info("Got the preprocesser object")
+
+            logging.info("Initializing transformation for training data.")
+            input_features_train_arr = preprocesser.fit_transform(input_features_train_df)
+            logging.info("Initalizing transformation for testing data.")
+            input_features_test_arr = preprocesser.transform(input_features_test_df)
+            logging.info("Transformation done end to end to train test df.")
+
+            logging.info("Applying SMOTEENN for handling imbalance dataset.")
+            smt = SMOTEENN(sampling_strategy = 'minority')
+            input_features_train_final, target_features_train_final = smt.fit_resample(
+                input_features_train_arr, target_features_train_df
+            )
+            input_features_test_final, target_features_test_final = smt.fit_resample(
+                input_features_test_arr, target_features_test_df
+            )
+            logging.info("SMOTEENN applied to train-test df.")
+
+            train_arr = np.c_[input_features_train_final, np.array(target_features_train_final)]
+            test_arr = np.c_[input_features_test_final, np.array(target_features_test_final)]
+            logging.info("feature-target concatenation done for train-test df.")
+
+            save_object(self.data_transformation_config.transformed_object_file_path,preprocesser)
+            save_numpy_array_data(self.data_transformation_config.transformed_train_file_path,array=train_arr)
+            save_numpy_array_data(self.data_transformation_config.transformed_test_file_path,array=test_arr)
+            logging.info("Saving transformation object and transformed files.")
+
+            logging.info("Data transformation completed successfully.")
+            return DataTransformationArtifact(
+                transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
+                transformed_train_file_path= self.data_transformation_config.transformed_train_file_path,
+                transformed_test_file_path= self.data_transformation_config.transformed_test_file_path
+            )
+
+        except Exception as e:
+            raise MyException(e,sys) from e
